87568bc45c3ec8d5701f43a62c738faab6d61c97
Adrianna <azdayo@gmail.com>
2024-03-04 12:12:06 +0100

    Comments view

--- frontend/src/sites/post.js
+++ frontend/src/sites/post.js
@@ -86,7 +86,6 @@ export default function Post(props) {
 
 
   useEffect(() => {
-    console.log(post_id);
     axios.get('/get_current_post/'+post_id, {
       headers: {
           'Accept': 'application/json'
@@ -95,9 +94,9 @@ export default function Post(props) {
     .then(response => {
       let result = response.data;
         if (result) {
-            setLoading(false);
-            setPost(result.data.post);
-            setComments(result.data.comments);
+          setLoading(false);
+          setPost(result.data.post);
+          setComments(result.data.comments);
         }
     })
     .catch( _ => {
@@ -140,7 +139,10 @@ export default function Post(props) {
                 <Divider orientation="horizontal"/>
 
                 <Card sx={{ display: 'flex', background: 'none', boxShadow: 'none', my: 2 }}>
-                  <Avatar sx={{ bgcolor: avatarContext[post.author.username[0].toUpperCase()], width: 50, height: 50, mx: 2, my: 4 }}> {post.author.username[0].toUpperCase()} </Avatar>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+                    <Avatar sx={{ bgcolor: avatarContext[post.author.username[0].toUpperCase()], width: 50, height: 50, mx: 2, mt: 4, mb: 1 }}> {post.author.username[0].toUpperCase()} </Avatar>
+                    <Typography variant="body2" sx={{ fontSize: '14px' }}>{post.author.username}</Typography>
+                  </Box>
                   <Box sx={{ display: 'flex', flexDirection: 'column', width: '55%', pt: 2 }}>
                     <CardContent>
                         <Typography variant="body1" style={{ fontSize: '16px', whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>{post.description}</Typography>
@@ -156,25 +158,51 @@ export default function Post(props) {
                 </Card>
 
                 {comments &&
-                  <Box>
+                  <Box sx={{ borderLeft: '8px solid #466485', my: 5, width: '80%' }}>
                     <Divider orientation="horizontal" />
-
-                    {/* {comments.forEach(comment => (
-                    ))} */}
+                    {comments.map(c => (
+                      <Box key={c.id}>
+                        <Card sx={{ display: 'flex', background: 'none', boxShadow: 'none', ml: 2, my: 2 }}>
+                          <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+                            <Avatar sx={{ bgcolor: avatarContext[c.author.username[0].toUpperCase()], width: 45, height: 45, mx: 2, mt: 4, mb: 1 }}> {c.author.username[0].toUpperCase()} </Avatar>
+                            <Typography variant="body2" sx={{ fontSize: '13px' }}>{c.author.username}</Typography>
+                          </Box>
+                          {/* <Avatar sx={{ bgcolor: avatarContext[c.author.username[0].toUpperCase()], width: 40, height: 40, mx: 2, my: 4 }}> {c.author.username[0].toUpperCase()} </Avatar> */}
+                          <Box sx={{ display: 'flex', flexDirection: 'column', width: '55%', pt: 2 }}>
+                            <CardContent>
+                              <Typography variant="body1" style={{ fontSize: '16px', whiteSpace: 'pre-wrap', overflowWrap: 'break-word', marginBottom: 10 }}>{c.content}</Typography>
+                              <Typography variant="overline">{dateFormat(c.created_at)}</Typography>
+                            </CardContent>
+                          </Box>
+                        </Card>
+                        <Divider orientation="horizontal" />
+                      </Box>
+                    ))}
                   </Box>
                 }
 
-                <Divider orientation="horizontal" />
-
-                <Box sx={{ width: '70%', py: 5 }} component="form" onSubmit={handleSubmit(onSubmit)}>
-                  <InputLabel htmlFor="description-input" sx={{ pb: 2 }}>Wprowadź poniżej treść swojego komentarza</InputLabel>
-                  <OutlinedInput id="description-input" color="primary" size="small" fullWidth multiline minRows={8}
-                    {...register("comment", { required: false })}
-                  />
-                  <Box sx={{ display: 'flex', mt: 3, mr: 3 }}>
-                    <StyledBasicButton sx={{ marginLeft: 'auto' }} type="submit" variant="contained" endIcon={<SendIcon />}>Dodaj komentarz</StyledBasicButton>
+                {!props.user &&
+                  <Box sx={{ width: '70%', py: 5 }} component="form" onSubmit={handleSubmit(onSubmit)}>
+                    <InputLabel htmlFor="description-input" sx={{ pb: 2 }}>Wprowadź poniżej treść swojego komentarza</InputLabel>
+                    <OutlinedInput id="description-input" disabled color="primary" size="small" fullWidth multiline minRows={8}
+                      defaultValue="Zaloguj się aby dodać komentarz"/>
+                    <Box sx={{ display: 'flex', mt: 3, mr: 3 }}>
+                      <StyledBasicButton sx={{ marginLeft: 'auto' }} disabled type="submit" variant="contained" endIcon={<SendIcon />}>Dodaj komentarz</StyledBasicButton>
+                    </Box>
                   </Box>
-                </Box>
+                }
+
+                {props.user &&
+                  <Box sx={{ width: '70%', py: 5 }} component="form" onSubmit={handleSubmit(onSubmit)}>
+                    <InputLabel htmlFor="description-input" sx={{ pb: 2 }}>Wprowadź poniżej treść swojego komentarza</InputLabel>
+                    <OutlinedInput id="description-input" color="primary" size="small" fullWidth multiline minRows={8}
+                      {...register("comment", { required: false })}
+                    />
+                    <Box sx={{ display: 'flex', mt: 3, mr: 3 }}>
+                      <StyledBasicButton sx={{ marginLeft: 'auto' }} type="submit" variant="contained" endIcon={<SendIcon />}>Dodaj komentarz</StyledBasicButton>
+                    </Box>
+                  </Box>
+                }
             </Grid>
         }
       </Grid>
